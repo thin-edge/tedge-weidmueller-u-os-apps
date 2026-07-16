5339eceddadac196cabd648ca60e23f31140a413
Kai Reinhardt <kai.reinhardt@cumulocity.com>
2026-07-16 11:12:13 +0200

    Finalize latest image, startup, and packaging updates

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ type TargetCredentials struct {
 
 const (
 	defaultImageName        = "u-os-app-thin-edge"
-	defaultImageVersion     = "2.0.1-8"
+	defaultImageVersion     = "2.0.1-10"
 	defaultUCAOMPackagerTag = "0.8.0"
 )
 
